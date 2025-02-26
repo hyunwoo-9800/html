0278bdf27ff90d26ad9571ae141fdfb9a53f2698
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-02-26 13:35:31 +0900

    <add> HTML_final_project 작업중 2025/02/26  @0.0h

--- final_project/js/scroll_request.js
+++ final_project/js/scroll_request.js
@@ -3,11 +3,14 @@ const headerEl = document.querySelector("header");
 window.addEventListener('scroll', function () {
   requestAnimationFrame(scrollCheck);
 });
+
 function scrollCheck() {
   let browerScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
+
   if (browerScrollY > 0) {
     headerEl.classList.add("active");
   } else {
     headerEl.classList.remove("active");
   }
+  
 }
