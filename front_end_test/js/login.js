f42882f5d6204a5f30a79fbe5597874c67d8b5ae
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-02-28 14:49:42 +0900

    <update> HTML_final_project 애니메이션 수정, 변수명, 함수명 수정 2025/02/28  @0.5h

--- front_end_test/js/login.js
+++ front_end_test/js/login.js
@@ -2,8 +2,8 @@
 function login() {
 
     // id값으로 입력값을 얻어와서 각각의 변수에 저장
-    var email = document.getElementById("login_emaliaddress").value;
-    var password = document.getElementById("login_pwd").value;
+    var email = document.getElementById("loginEmaliaddress").value;
+    var password = document.getElementById("loginPwd").value;
 
     // 입력체크 시작
     if (email == null || email == "") {
@@ -17,7 +17,7 @@ function login() {
     }
 
     // 이메일 형식이며, 패스워드가 입력되어 있다면 문구를 출력 후 페이지 이동
-    if (email_check() == true) {
+    if (emailCheck() == true) {
         if (password != null && password != "") {
             alert(email + "님 환영합니다!");
             location.href = 'main.html';
@@ -29,10 +29,10 @@ function login() {
 
 // input타입 required속성을 대신해서 입력확인을 할 함수
 // 정규식으로 이메일 형식 체크
-function email_check() {
-    var email = document.getElementById("login_emaliaddress").value;
-    var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-    if (!email_regex.test(email)) {
+function emailCheck() {
+    var email = document.getElementById("loginEmaliaddress").value;
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if (!emailRegex.test(email)) {
         alert("이메일 형식에 맞게 적어주세요.");
         return false;
     } else {
@@ -41,10 +41,10 @@ function email_check() {
 }
 
 // 페이지 이동용 함수
-function go_join() {
+function goJoin() {
     location.href = 'signup.html';
 }
 
-function go_login() {
+function goLogin() {
     location.href = 'login.html';
 }
--- front_end_test/js/sign.js
+++ front_end_test/js/sign.js
@@ -4,7 +4,7 @@ function join() {
     // id값으로 입력값을 얻어와서 각각의 변수에 저장
     var email = document.getElementById("emailAddress").value;
     var password = document.getElementById("pwd").value;
-    var confirm = document.getElementById("confirm_pwd").value;
+    var confirm = document.getElementById("confirmPwd").value;
 
     // 입력체크 시작
     if (email == null || email == "") {
@@ -28,7 +28,7 @@ function join() {
     }
 
     // 이메일이 정확하고, 패스워드가 공란이거나 null값이 아니면 회원가입 처리
-    if (email_check() == true) {
+    if (emailCheck() == true) {
         if (password != null && password != "") {
             alert("회원가입이 완료되었습니다.")
         }
@@ -38,10 +38,10 @@ function join() {
 
 // input타입 required속성을 대신해서 입력확인을 할 함수
 // 정규식으로 이메일 형식 체크
-function email_check() {
+function emailCheck() {
     var email = document.getElementById("emailAddress").value;
-    var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-    if (!email_regex.test(email)) {
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if (!emailRegex.test(email)) {
         alert("이메일 형식에 맞게 적어주세요.");
         return false;
     } else {
@@ -50,10 +50,10 @@ function email_check() {
 }
 
 // 페이지 이동용 함수
-function go_join() {
+function goJoin() {
     location.href = 'signup.html';
 }
 
-function go_login() {
+function goLogin() {
     location.href = 'login.html';
 }
