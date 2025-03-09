e17e4110b3f95e1186839ad70a74b1d3a4a7195b
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-03-09 11:21:40 +0900

    [update] final project 사진 추가 및 주석 추가

---
+++ final_project/js/link.js
@@ -0,0 +1,21 @@
+// 전체 페이지에서 헤더영역 클릭시 이동시킬 js
+
+function goMain() {
+    location.href = "main.html";
+}
+
+function goAbout() {
+    location.href = "about_me.html";
+}
+
+function goFeatures() {
+    location.href = "what_i_do.html";
+}
+
+function goPortifolio() {
+    location.href = "portfolio.html";
+}
+
+function goContact() {
+    location.href = "contact_with_me.html";
+}
